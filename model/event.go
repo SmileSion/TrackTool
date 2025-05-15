0202579c0c4f5c5c6ab14bb2fb0898b3ce871880
SmileSion <1065716478@qq.com>
2025-05-15 10:05:02 +0800

    字段调整

--- model/event.go
+++ model/event.go
@@ -2,12 +2,12 @@ package model
 
 // Event 事件结构体
 type Event struct {
-	ClientType  string `json:"client_type"`
+	ClientType  string `json:"clientType"`
 	Site        string `json:"site"`
-	EventType   string `json:"event_type"`
-	EventDetail string `json:"event_detail"`
-	UserDetail  string `json:"user_detail"`
+	EventType   string `json:"eventType"`
+	EventDetail string `json:"eventDetail"`
+	UserDetail  string `json:"userDetail"`
 	Timestamp   int64  `json:"timestamp"`    // 后端生成的时间戳
-	TimeStamp   int64  `json:"time_stamp"`   // 前端传入的时间戳（用于校验，仅存入Redis）
+	TimeStamp   int64  `json:"timeStamp"`   // 前端传入的时间戳（用于校验，仅存入Redis）
 }
 
